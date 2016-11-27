99750c9075e649ef0df9207410b0e0095ee9babe
vitimario <vitimario2@gmail.com>
2016-11-27 15:54:11 +0100

    added MetropolisHasting method, not finished

--- montecarlo/mcmc.py
+++ montecarlo/mcmc.py
@@ -14,10 +14,13 @@ def pi_monteCarlo(N):
     x = np.zeros((N,))
     y = np.zeros((N,))
     for i in range(N):
+        # step 1: generate a sample from the square.
         x[i] = uniform(-1,1)
         y[i] = uniform(-1,1)
         d = (x[i]**2 + y[i]**2)
+        # step 2: apply a selection rule.
         if np.abs(d) <= 1:
             in_pi +=1
+    # step 3: average to estimate expected value.
     pi = 4*( in_pi/float(N) )
     return pi, x, y
